a5145656f590b16c1b0edb29a04efdfe85988f6b
Felix Li <future.doo@gmail.com>
2025-11-19 22:16:59 -0600

    Update version of @fli5/react-library to beta and replace Button with Label in App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Button} from '@fli5/react-library';
+import { Label} from '@fli5/react-library';
 
 function App() {
   return (
     <div>
-      <Button text='Hello Felix'></Button>
+      <Label text='Hello Felix'></Label>
     </div>
   );
 }
